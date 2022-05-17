0358f1586e24b627d5e704e16429d48d94cb2972
hem_b <hemanthb93@gmail.com>
2022-05-17 15:11:09 +0530

    [Hemanth]: added 3 play options using switch case in a method

--- SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 SnakeAndLadderGame.SnakeAndLadder snakeAndLadder = new SnakeAndLadderGame.SnakeAndLadder();
 Console.WriteLine("Dice number obtained in rolling is --- " + snakeAndLadder.DiceRoll());
+snakeAndLadder.PlayOptions();
--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
@@ -9,11 +9,39 @@ namespace SnakeAndLadderGame
     internal class SnakeAndLadder
     {
         int diceNo,playerPostion=0,playerStartPosition = 0;
+        const int noPlay = 0, ladder = 1, snake = 2;
         Random random = new Random();
         public int DiceRoll()
         {
             diceNo = random.Next(1,7);
             return diceNo;
+        }
+
+        public void PlayOptions()
+        {
+            switch (random.Next(0, 3))
+            {
+                case noPlay:
+                    playerPostion += 0;
+                    break;
+                case ladder:
+                    playerPostion += diceNo;
+                    break;
+                case snake:
+                    playerPostion -= diceNo;
+                    break ;
+                 default:
+                    break;
+            }
+            if (playerPostion < 0)
+            {
+                playerPostion = 0;
+                Console.WriteLine("Player Position is " + playerPostion);
+            }
+            else
+            {
+                Console.WriteLine("Player Position is " + playerPostion);
+            }
         }
     }
 }
