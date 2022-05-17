11c4788599c93644dbd0377fe19f82dec36e4224
hem_b <hemanthb93@gmail.com>
2022-05-17 15:30:23 +0530

    [Hemanth]:Dice roll untill player reaches 100th pos and reset to 0 if position goes below 0

--- SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/SnakeAndLadder.cs
@@ -8,7 +8,7 @@ namespace SnakeAndLadderGame
 {
     internal class SnakeAndLadder
     {
-        int diceNo,playerPostion=0,playerStartPosition = 0;
+        int diceNo,playerPosition=0,playerStartPosition = 0;
         const int noPlay = 0, ladder = 1, snake = 2;
         Random random = new Random();
         public int DiceRoll()
@@ -19,29 +19,37 @@ namespace SnakeAndLadderGame
 
         public void PlayOptions()
         {
-            switch (random.Next(0, 3))
+            while (playerPosition <= 100)
             {
-                case noPlay:
-                    playerPostion += 0;
+                switch (random.Next(0, 3))
+                {
+                    case noPlay:
+                        playerPosition += 0;
+                        break;
+                    case ladder:
+                        playerPosition += DiceRoll();
+                        break;
+                    case snake:
+                        playerPosition -= DiceRoll();
+                        break;
+                    default:
+                        break;
+                }
+                if (playerPosition < 0)
+                {
+                    playerPosition = 0;
+                    //Console.WriteLine("Player Position is " + playerPostion);
+                }
+                Console.WriteLine("Player Position is "+playerPosition);
+                if (playerPosition == 100)
+                {
                     break;
-                case ladder:
-                    playerPostion += diceNo;
-                    break;
-                case snake:
-                    playerPostion -= diceNo;
-                    break ;
-                 default:
-                    break;
-            }
-            if (playerPostion < 0)
-            {
-                playerPostion = 0;
-                Console.WriteLine("Player Position is " + playerPostion);
-            }
-            else
-            {
-                Console.WriteLine("Player Position is " + playerPostion);
-            }
+                }
+                    /*else
+                    {
+                        Console.WriteLine("Player Position is " + playerPostion);
+                    }*/
+                }
         }
     }
 }
