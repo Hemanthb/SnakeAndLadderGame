b3c1e0a18d04025bf57dc9bbedf086c9c2e66608
hem_b <hemanthb93@gmail.com>
2022-05-18 14:27:30 +0530

    [Hemanth]: snake&Ladder game updated as a 2 player game

---
+++ SnakeAndLadderGame/SnakeAndLadderGame/MultiplePlayersGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    class MultiplePlayersGame
+    {
+        public String playerName;
+        public int diceNo, playerPosition = 0;
+        const int noPlay = 0, ladder = 1, snake = 2;
+        Random random = new Random();
+        public MultiplePlayersGame(string playerName)
+        {
+            this.playerName = playerName;
+        }
+
+
+        public int DiceRoll()
+        {
+            diceNo = random.Next(1, 7);
+            return diceNo;
+        }
+        public void Play()
+        {
+        repeat:
+            int option = random.Next(3);
+            switch (option)
+            {
+                case noPlay:
+                    playerPosition += 0;
+                    
+                    break;
+                case ladder:
+                    playerPosition += DiceRoll();
+                    break;
+                case snake:
+                    playerPosition -= DiceRoll();
+                    break;
+                default:
+                    break;
+            }
+            if (playerPosition < 0)
+            {
+                playerPosition = 0;
+                Console.Write(playerName + " Position is " + playerPosition);
+            }
+            else if (playerPosition > 100)
+            {
+                playerPosition -= diceNo;
+                Console.Write(playerName + " Position is " + playerPosition);
+            }
+            else
+            {
+                Console.Write(playerName + " Position is " + playerPosition);
+            }
+            if (option == ladder)
+            {
+                if (playerPosition != 100)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("- - - - - - - - - - - - - - -");
+                    Console.WriteLine(playerName+"Got to roll dice again as its ladder");
+                    goto repeat;
+                }
+            }
+        }
+
+    }
+}
--- SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/SnakeAndLadderGame/Program.cs
@@ -1,5 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 SnakeAndLadderGame.SnakeAndLadder snakeAndLadder = new SnakeAndLadderGame.SnakeAndLadder();
+SnakeAndLadderGame.MultiplePlayersGame multiplePlayers1 = new SnakeAndLadderGame.MultiplePlayersGame("Player1");
+SnakeAndLadderGame.MultiplePlayersGame multiplePlayers2 = new SnakeAndLadderGame.MultiplePlayersGame("Player2");
+
 Console.WriteLine("Dice number obtained in rolling is --- " + snakeAndLadder.DiceRoll());
 snakeAndLadder.PlayOptions();
 
+while(multiplePlayers1.playerPosition<100 && multiplePlayers2.playerPosition < 100)
+{
+    multiplePlayers1.Play();
+
+    if (multiplePlayers1.playerPosition == 100)
+        {
+            Console.Write("Player1 won");
+            break;
+        }
+        
+    Console.Write("  ||  ");
+    multiplePlayers2.Play();
+
+    if (multiplePlayers2.playerPosition == 100)
+        {
+            Console.Write("Player2 won");
+        }
+    Console.WriteLine();
+    
+}
